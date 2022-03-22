57ad1ec33308bdcb569d73d09186f60fdf7e09e8
Angelene Manalo <angelenelm@gmail.com>
2022-03-22 10:47:53 -0700

    removed styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect, useLocation } from "react-router-dom";
 import { accessToken, logout, getCurrentUserProfile } from "./myanimelist";
 import { catchErrors } from "./utils";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { GlobalStyle } from "./styles";
-import { Login } from "./pages";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -16,12 +12,6 @@ function ScrollToTop() {
 
   return null;
 }
-=======
-import "./App.css";
->>>>>>> parent of 1d69b01 (initial globalstyle)
-=======
-import "./App.css";
->>>>>>> parent of 1d69b01 (initial globalstyle)
 
 function App() {
   const [token, setToken] = useState(null);
@@ -32,7 +22,7 @@ function App() {
 
     const fetchData = async () => {
       const { data } = await getCurrentUserProfile();
-      
+
       if (data.status !== 401) {
         setProfile(data);
       }
@@ -42,16 +32,11 @@ function App() {
   }, []);
 
   return (
-    
     <div className="App">
       <header className="App-header">
         <Router>
           <Switch>
-            
             <Route exact path="/">
-<<<<<<< HEAD
-              {!token || !profile ? <Login /> : <Redirect to="/:id" />}
-=======
               {!token || !profile ? (
                 <>
                   <a className="App-link" href="http://localhost:8080/login">
@@ -61,33 +46,29 @@ function App() {
               ) : (
                 <Redirect to="/:id" />
               )}
->>>>>>> parent of 1d69b01 (initial globalstyle)
             </Route>
 
             <Route exact path="/:id">
-              
               <button onClick={logout}> Log out</button>
               <h1>{profile?.name}</h1>
               <p>Joined at {profile?.joined_at}</p>
               <img src={profile?.picture} alt="Avatar" />
-
             </Route>
-              
+
             <Route exact path="/:id/anime">
               <h1>All time anime stats</h1>
             </Route>
             <Route exact path="/:id/anime/:id">
-                <h1>[year] anime stats</h1>
+              <h1>[year] anime stats</h1>
             </Route>
 
             <Route exact path="/:id/manga/">
-                <h1>[year] manga stats</h1>
+              <h1>[year] manga stats</h1>
             </Route>
 
             <Route exact path="/:id/manga/:id">
-                <h1>[year] manga stats</h1>
+              <h1>[year] manga stats</h1>
             </Route>
-            
           </Switch>
         </Router>
       </header>
--- client/src/styles/index.js
+++
@@ -1,3 +0,0 @@
-export { default as GlobalStyle } from "./GlobalStyle";
-export { default as fonts } from "./fonts";
-export { default as theme } from "./variables";
